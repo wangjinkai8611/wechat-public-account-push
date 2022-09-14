435dcdf2da46204993c47544825edc23a9aed703
wangjinkai8611 <71968308+wangjinkai8611@users.noreply.github.com>
2022-09-14 15:43:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -37,7 +37,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.1212.com/luck/cancer/20220914.html",
+      openUrl: "https://wangxinleo.cn",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -65,7 +65,7 @@ export const config = {
       city: "焦作",
       horoscopeDate: '',
       horoscopeDateType: '',
-      openUrl: "https://www.1212.com/luck/cancer/20220914.html",
+      openUrl: "https://wangxinleo.cn",
       festivals: [
         {"type": "生日", "name": "菲菲宝宝", "year": "1997", "date": "07-14"},
         {"type": "生日", "name": "milky", "year": "2020", "date": "04-20"},
